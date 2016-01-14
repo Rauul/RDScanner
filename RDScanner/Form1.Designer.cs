205e8e60e1cc5f7216f70a45da800aee8caf956e
Vindis <martin.vindis.80@gmail.com>
2016-01-14 02:46:29 +0100

    First

---
+++ RDScanner/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace RDScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.carsButton = new System.Windows.Forms.Button();
+            this.tracksButton = new System.Windows.Forms.Button();
+            this.skinsButton = new System.Windows.Forms.Button();
+            this.miscButton = new System.Windows.Forms.Button();
+            this.allButton = new System.Windows.Forms.Button();
+            this.carsLabel = new System.Windows.Forms.Label();
+            this.miscLabel = new System.Windows.Forms.Label();
+            this.skinsLabel = new System.Windows.Forms.Label();
+            this.tracksLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // carsButton
+            // 
+            this.carsButton.Location = new System.Drawing.Point(13, 13);
+            this.carsButton.Name = "carsButton";
+            this.carsButton.Size = new System.Drawing.Size(75, 23);
+            this.carsButton.TabIndex = 0;
+            this.carsButton.Text = "Cars";
+            this.carsButton.UseVisualStyleBackColor = true;
+            this.carsButton.Click += new System.EventHandler(this.carsButton_Click);
+            // 
+            // tracksButton
+            // 
+            this.tracksButton.Location = new System.Drawing.Point(95, 13);
+            this.tracksButton.Name = "tracksButton";
+            this.tracksButton.Size = new System.Drawing.Size(75, 23);
+            this.tracksButton.TabIndex = 1;
+            this.tracksButton.Text = "Tracks";
+            this.tracksButton.UseVisualStyleBackColor = true;
+            this.tracksButton.Click += new System.EventHandler(this.tracksButton_Click);
+            // 
+            // skinsButton
+            // 
+            this.skinsButton.Location = new System.Drawing.Point(177, 13);
+            this.skinsButton.Name = "skinsButton";
+            this.skinsButton.Size = new System.Drawing.Size(75, 23);
+            this.skinsButton.TabIndex = 2;
+            this.skinsButton.Text = "Skins";
+            this.skinsButton.UseVisualStyleBackColor = true;
+            this.skinsButton.Click += new System.EventHandler(this.skinsButton_Click);
+            // 
+            // miscButton
+            // 
+            this.miscButton.Location = new System.Drawing.Point(259, 13);
+            this.miscButton.Name = "miscButton";
+            this.miscButton.Size = new System.Drawing.Size(75, 23);
+            this.miscButton.TabIndex = 3;
+            this.miscButton.Text = "Misc";
+            this.miscButton.UseVisualStyleBackColor = true;
+            this.miscButton.Click += new System.EventHandler(this.miscButton_Click);
+            // 
+            // allButton
+            // 
+            this.allButton.Location = new System.Drawing.Point(13, 71);
+            this.allButton.Name = "allButton";
+            this.allButton.Size = new System.Drawing.Size(321, 67);
+            this.allButton.TabIndex = 4;
+            this.allButton.Text = "ALL";
+            this.allButton.UseVisualStyleBackColor = true;
+            this.allButton.Click += new System.EventHandler(this.allButton_Click);
+            // 
+            // carsLabel
+            // 
+            this.carsLabel.AutoSize = true;
+            this.carsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.carsLabel.Location = new System.Drawing.Point(43, 39);
+            this.carsLabel.Name = "carsLabel";
+            this.carsLabel.Size = new System.Drawing.Size(17, 17);
+            this.carsLabel.TabIndex = 5;
+            this.carsLabel.Text = "0";
+            // 
+            // miscLabel
+            // 
+            this.miscLabel.AutoSize = true;
+            this.miscLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.miscLabel.Location = new System.Drawing.Point(286, 39);
+            this.miscLabel.Name = "miscLabel";
+            this.miscLabel.Size = new System.Drawing.Size(17, 17);
+            this.miscLabel.TabIndex = 8;
+            this.miscLabel.Text = "0";
+            // 
+            // skinsLabel
+            // 
+            this.skinsLabel.AutoSize = true;
+            this.skinsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.skinsLabel.Location = new System.Drawing.Point(207, 39);
+            this.skinsLabel.Name = "skinsLabel";
+            this.skinsLabel.Size = new System.Drawing.Size(17, 17);
+            this.skinsLabel.TabIndex = 9;
+            this.skinsLabel.Text = "0";
+            // 
+            // tracksLabel
+            // 
+            this.tracksLabel.AutoSize = true;
+            this.tracksLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tracksLabel.Location = new System.Drawing.Point(126, 39);
+            this.tracksLabel.Name = "tracksLabel";
+            this.tracksLabel.Size = new System.Drawing.Size(17, 17);
+            this.tracksLabel.TabIndex = 10;
+            this.tracksLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 150);
+            this.Controls.Add(this.tracksLabel);
+            this.Controls.Add(this.skinsLabel);
+            this.Controls.Add(this.miscLabel);
+            this.Controls.Add(this.carsLabel);
+            this.Controls.Add(this.allButton);
+            this.Controls.Add(this.miscButton);
+            this.Controls.Add(this.skinsButton);
+            this.Controls.Add(this.tracksButton);
+            this.Controls.Add(this.carsButton);
+            this.Name = "Form1";
+            this.Text = "RDScanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button carsButton;
+        private System.Windows.Forms.Button tracksButton;
+        private System.Windows.Forms.Button skinsButton;
+        private System.Windows.Forms.Button miscButton;
+        private System.Windows.Forms.Button allButton;
+        private System.Windows.Forms.Label carsLabel;
+        private System.Windows.Forms.Label miscLabel;
+        private System.Windows.Forms.Label skinsLabel;
+        private System.Windows.Forms.Label tracksLabel;
+    }
+}
+
---
+++ RDScanner/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Net.Mime;
+
+namespace RDScanner
+{
+    public partial class Form1 : Form
+    {
+        XmlFile xmlFile = new XmlFile();
+        List<Mod> carsList = new List<Mod>();
+        List<Mod> tracksList = new List<Mod>();
+        List<Mod> skinsList = new List<Mod>();
+        List<Mod> miscList = new List<Mod>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            xmlFile.name = "RDFiles.xml";
+            xmlFile.path = @"C:\Users\Martin\Dropbox\RDScanner";
+            xmlFile.fullName = @"C:\Users\Martin\Dropbox\RDScanner\RDFiles.xml";
+
+            if (!File.Exists(xmlFile.fullName))
+                CreateXmlFile(xmlFile);
+        }
+
+        private void carsButton_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => GetModList("cars"));
+        }
+
+        private void tracksButton_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => GetModList("tracks"));
+        }
+
+        private void skinsButton_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => GetModList("skins"));
+        }
+
+        private void miscButton_Click(object sender, EventArgs e)
+        {
+            Task.Run(() => GetModList("misc"));
+        }
+
+        private void allButton_Click(object sender, EventArgs e)
+        {
+            CreateXmlFile(xmlFile);
+            Task.Run(() => GetModList("cars"));
+            Task.Run(() => GetModList("tracks"));
+            Task.Run(() => GetModList("skins"));
+            Task.Run(() => GetModList("misc"));
+        }
+
+        public void GetModList(string type)
+        {
+            lock(this)
+            {
+                if (!File.Exists(xmlFile.fullName))
+                    CreateXmlFile(xmlFile);
+
+                string url = "";
+                if (type == "cars")
+                    url = "http://www.racedepartment.com/downloads/categories/sce-cars.82/";
+                else if (type == "tracks")
+                    url = "http://www.racedepartment.com/downloads/categories/sce-tracks.22/";
+                else if (type == "skins")
+                    url = "http://www.racedepartment.com/downloads/categories/sce-skins.23/";
+                else if (type == "misc")
+                    url = "http://www.racedepartment.com/downloads/categories/sce-misc.24/";
+                WebClient wc = new WebClient();
+                string html = wc.DownloadString(url);
+
+                MatchCollection mColl = Regex.Matches(html, "<span class=\"pageNavHeader\">Page 1 of (.+?)</span>", RegexOptions.Singleline);
+                int pageCount = Convert.ToInt32(mColl[0].Groups[1].Value);
+
+                for (int i = 1; i <= pageCount; i++)
+                {
+                    html = wc.DownloadString(url + "?page=" + i);
+                    MatchCollection mName = Regex.Matches(html, "<h3 class=\"title\">\\s*(.+?)<div class=\"listBlock resourceStats\">", RegexOptions.Singleline);
+
+                    foreach (Match m in mName)
+                    {
+                        Mod mod = new Mod();
+                        string substring = m.Groups[1].Value;
+                        string pat;
+                        string subhtml;
+                        Regex r;
+                        Match submatch;
+
+                        pat = ">(.+?)</a>";
+                        r = new Regex(pat, RegexOptions.Singleline);
+                        submatch = r.Match(substring);
+                        mod.name = HttpUtility.HtmlDecode(submatch.Groups[1].Value);
+
+
+                        pat = "\"version\">(.+?)</span>";
+                        r = new Regex(pat, RegexOptions.Singleline);
+                        submatch = r.Match(substring);
+                        mod.version = submatch.Groups[1].Value;
+
+                        pat = "\"auto\">(.+?)</a>";
+                        r = new Regex(pat, RegexOptions.Singleline);
+                        submatch = r.Match(substring);
+                        mod.author = submatch.Groups[1].Value;
+
+                        pat = "([a-zA-Z]{3}\\s[0-9]{1,2},\\s[0-9]{4})";
+                        r = new Regex(pat, RegexOptions.Singleline);
+                        submatch = r.Match(substring);
+                        mod.date = submatch.Groups[1].Value;
+
+                        pat = "\"(.+?)/\"";
+                        r = new Regex(pat, RegexOptions.Singleline);
+                        submatch = r.Match(substring);
+
+                        subhtml = wc.DownloadString("http://www.racedepartment.com/" + submatch.Groups[1].Value);
+
+                        pat = "<label\\sclass=\"downloadButton\\s\">\\s*<a\\shref=\"(.+?)\"";
+                        r = new Regex(pat, RegexOptions.Singleline);
+                        submatch = r.Match(subhtml);
+                        mod.downloadLink = "http://www.racedepartment.com/" + submatch.Groups[1].Value;
+
+                        mod.fileName = GetFileName(mod.downloadLink);
+
+                        pat = "<small\\sclass=\"minorText\">(.+?)\\sMB";
+                        r = new Regex(pat, RegexOptions.Singleline);
+                        submatch = r.Match(subhtml);
+                        mod.size = submatch.Groups[1].Value;
+
+                        if (mod.size == "")
+                            mod.size = "<1.0";
+
+                        mod.type = type;
+
+                        NewXmlEntry(mod);
+                        switch (type)
+                        {
+                            case "cars":
+                                carsLabel.Invoke((MethodInvoker)delegate { carsLabel.Text = (Convert.ToInt32(carsLabel.Text) + 1).ToString(); });
+                                break;
+                            case "tracks":
+                                tracksLabel.Invoke((MethodInvoker)delegate { tracksLabel.Text = (Convert.ToInt32(tracksLabel.Text) + 1).ToString(); });
+                                break;
+                            case "skins":
+                                skinsLabel.Invoke((MethodInvoker)delegate { skinsLabel.Text = (Convert.ToInt32(skinsLabel.Text) + 1).ToString(); });
+                                break;
+                            case "misc":
+                                miscLabel.Invoke((MethodInvoker)delegate { miscLabel.Text = (Convert.ToInt32(miscLabel.Text) + 1).ToString(); });
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private string GetFileName(string downloadLink)
+        {
+            string fileName = "";
+            using (WebClient wc = new WebClient())
+            {
+                Uri url = new Uri(downloadLink);
+                Stream myStream = wc.OpenRead(downloadLink);
+                string header_contentDisposition = wc.ResponseHeaders["content-disposition"];
+                fileName = new ContentDisposition(header_contentDisposition).FileName;
+                myStream.Close();
+            }
+            return fileName;
+        }
+
+        private void CreateXmlFile(XmlFile xmlFile)
+        {
+            Directory.CreateDirectory(xmlFile.path);
+            //File.Create(xmlFile.fullName);
+            XDocument xmlDoc = new XDocument(
+                new XElement("root",
+                new XElement("cars"),
+                new XElement("tracks"),
+                new XElement("skins"),
+                new XElement("misc")
+                )
+                );
+            xmlDoc.Save(xmlFile.fullName);
+        }
+
+        private void NewXmlEntry(Mod mod)
+        {
+            XDocument xmlDoc = XDocument.Load(xmlFile.fullName);
+            XElement cars = xmlDoc.Element("root").Element("cars");
+            XElement tracks = xmlDoc.Element("root").Element("tracks");
+            XElement skins = xmlDoc.Element("root").Element("skins");
+            XElement misc = xmlDoc.Element("root").Element("misc");
+            switch (mod.type)
+            {
+                case "cars":
+                    cars.Add(new XElement("mod",
+                        new XElement("name", mod.name),
+                        new XElement("version", mod.version),
+                        new XElement("author", mod.author),
+                        new XElement("downloadLink", mod.downloadLink),
+                        new XElement("fileName", mod.fileName),
+                        new XElement("size", mod.size),
+                        new XElement("date", mod.date)));
+                    break;
+                case "tracks":
+                    tracks.Add(new XElement("mod",
+                        new XElement("name", mod.name),
+                        new XElement("version", mod.version),
+                        new XElement("author", mod.author),
+                        new XElement("downloadLink", mod.downloadLink),
+                        new XElement("fileName", mod.fileName),
+                        new XElement("size", mod.size),
+                        new XElement("date", mod.date)));
+                    break;
+                case "skins":
+                    skins.Add(new XElement("mod",
+                        new XElement("name", mod.name),
+                        new XElement("version", mod.version),
+                        new XElement("author", mod.author),
+                        new XElement("downloadLink", mod.downloadLink),
+                        new XElement("fileName", mod.fileName),
+                        new XElement("size", mod.size),
+                        new XElement("date", mod.date)));
+                    break;
+                case "misc":
+                    misc.Add(new XElement("mod",
+                        new XElement("name", mod.name),
+                        new XElement("version", mod.version),
+                        new XElement("author", mod.author),
+                        new XElement("downloadLink", mod.downloadLink),
+                        new XElement("fileName", mod.fileName),
+                        new XElement("size", mod.size),
+                        new XElement("date", mod.date)));
+                    break;
+                default:
+                    break;
+            }
+            xmlDoc.Save(xmlFile.fullName);
+        }
+    }
+
+    public class XmlFile
+    {
+        public string   name,
+                        path,
+                        fullName;
+    }
+
+    public class Mod
+    {
+        public string   name,
+                        version,
+                        author,
+                        downloadLink,
+                        fileName,
+                        size,
+                        type,
+                        date;
+    }
+}
